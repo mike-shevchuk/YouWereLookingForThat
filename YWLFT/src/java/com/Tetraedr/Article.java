2a74427d2b0532f7cb10fe4f1517609a9b54b92e
ShevaTetraedrProduction <tetraedrgo@gmail.com>
2020-02-22 10:50:22 +0200

    catch post ajax  message

--- YWLFT/src/java/com/Tetraedr/Article.java
+++ YWLFT/src/java/com/Tetraedr/Article.java
@@ -1,30 +1,28 @@
 package com.Tetraedr;
 
+import lombok.Data;
+
+@Data
 public class Article {
     private long id;
     private String content;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
+    public long getId() {  return id; }
+    public void setId(long id) {        this.id = id;    }
+    public String getContent() {        return content;    }
+    public void setContent(String content) {        this.content = content;}
+    public Article() {  }
+    public Article(long id, String content) {
         this.id = id;
+        this.content = content;
     }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {        this.content = content;    }
 
     @Override
     public String toString() {
-        System.out.println("Інформація яку перехопив java id = " + id +
-                " Повідомлення "+ content);
+        //System.out.println("Інформація яку перехопив java id = " + id +  " Повідомлення "+ content);
         return "Article{" +
-                "NEW id=" + id +
-                ", content='" + content + '\'' +
+                "NEW id = " + id +
+                ", content = '" + content + '\'' +
                 '}';
     }
 }
--- YWLFT/src/java/com/Tetraedr/ArticleController.java
+++ YWLFT/src/java/com/Tetraedr/ArticleController.java
@@ -1,44 +1,29 @@
 package com.Tetraedr;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/articles")
 public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
-
-    @GetMapping
-    public String getArticleForm() {
-        System.out.println("getArticleForm()");
-        return "popup.html";
-    }
-
-    //@PutMapping("/{id}")
     //@RequestMapping(value = "/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    //
-    @PostMapping("/{id}")
+    //@PostMapping(value = "/{id}", consumes = "application/json", produces = "application/json") //!SyntaxError: Unexpected token A in JSON at position 0
+    //@PostMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=application/json")//! Error 415.
+    //@PostMapping( "/{id}")    //! catch null value
+    @PostMapping(value = "/{content}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String postJson(@ModelAttribute Article article) {
+        //public String postJson(@RequestBody Article article) {   //!Error415
         System.out.println(article.getContent());
-        System.out.println(article.toString());
-        articleService.saveArticle(article);
-        System.out.println("added");
+        System.out.println("_______________________________");
+        // System.out.println(article.toString());
+        // articleService.saveArticle(article);
+        //System.out.println("added");
         return "Article created.";
     }
 
-/*
-    @GetMapping("/{id}")
-    public String getArticle(@PathVariable("id") long id, Model model) {
-        System.out.println("getArticle()");
-        Article article = articleService.getArticleById(id);
-        System.out.println(article.toString());
-        System.out.println("\n\n\n");
-        model.addAttribute("article", article);
-        return "article-page";
-    }
- */
 }
